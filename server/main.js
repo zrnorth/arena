4c7443bddc9990afd87a433cbcd9acca9800d392
Zach North <znorth@riotgames.com>
2016-11-08 15:58:55 -0800

    got the sockets talking now

--- server/main.js
+++ server/main.js
@@ -8,3 +8,12 @@ var io = require('socket.io')(server);
 
 // start'er up
 server.listen(port);
+
+// Handle game connections here
+io.on('connection', function(socket) {
+  console.log('hello dere');
+  socket.emit('news', { hello: 'world' });
+  socket.on('hello', function(data) {
+    console.log(data);
+  });
+});
